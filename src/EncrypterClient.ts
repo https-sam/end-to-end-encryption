48408977ce9f3058905cff499f5d657381dd23b1
Sam Goto <osgo1115@icloud.com>
2023-03-25 20:49:44 -0600

    e2ee object

---
+++ src/EncrypterClient.ts
@@ -0,0 +1,149 @@
+import crypto, { webcrypto } from "crypto";
+import { BufferSource } from "stream/web";
+
+export class E2EEClient {
+	private _privateKey: webcrypto.CryptoKey | undefined;
+	private _clientPublicKey: webcrypto.CryptoKey | undefined;
+	public publicKey: webcrypto.CryptoKey | undefined;
+
+	constructor() {}
+
+	public async init(): Promise<void> {
+		return new Promise((resolve, reject) => {
+			webcrypto.subtle
+				.generateKey(
+					{
+						name: "RSA-OAEP",
+						modulusLength: 2048,
+						publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
+						hash: { name: "SHA-256" },
+					},
+					true, // exportable
+					["encrypt", "decrypt"]
+				)
+				.then((key) => {
+					const { publicKey, privateKey } = key;
+					this.publicKey = publicKey;
+					this._privateKey = privateKey;
+					resolve();
+				})
+				.catch((err) => {
+					console.error(err);
+					this.publicKey = undefined;
+					reject();
+				});
+		});
+	}
+
+	/**
+	 * Exports this object's public key so that the client can
+	 * encrypt their data using this object's public key
+	 */
+	public async exportPublicKey(): Promise<ArrayBuffer | undefined> {
+		return new Promise((resolve, reject) => {
+			if (!this.publicKey) return reject(undefined);
+			crypto.subtle
+				.exportKey("spki", this.publicKey)
+				.then((keyBuffer) => {
+					resolve(keyBuffer);
+				})
+				.catch((err) => {
+					console.error(err);
+					reject(err);
+				});
+		});
+	}
+
+	/**
+	 * Loads the ArrayBuffer representing the client's public key
+	 * to this Object
+	 */
+	public async loadClientPublic(
+		clientPublicBuffer: ArrayBuffer
+	): Promise<void | string> {
+		return new Promise((resolve, reject) => {
+			webcrypto.subtle
+				.importKey(
+					"spki",
+					clientPublicBuffer,
+					{
+						name: "RSA-OAEP",
+						hash: { name: "SHA-256" },
+					},
+					false,
+					["encrypt"]
+				)
+				.then((importedPublic) => {
+					this._clientPublicKey = importedPublic;
+					resolve();
+				})
+				.catch((e) => reject(e));
+		});
+	}
+
+	/**
+	 * Encrypts BufferSource using the client's public key
+	 */
+	public encrypt(data: BufferSource): Promise<ArrayBuffer | string> {
+		return new Promise((resolve, reject) => {
+			if (!this._clientPublicKey) return reject("No client public key found");
+			webcrypto.subtle
+				.encrypt({ name: "RSA-OAEP" }, this._clientPublicKey, data)
+				.then((encryptedData: ArrayBuffer) => {
+					console.log("Succesfully enctypted");
+					resolve(encryptedData);
+				})
+				.catch((e) => {
+					console.log("Error while encrypting: ", e);
+					reject(e);
+				});
+		});
+	}
+
+	/**
+	 * Decrypts ArrayBuffer using the private key
+	 */
+	public decrypt(data: ArrayBuffer): Promise<ArrayBuffer | undefined> {
+		return new Promise((resolve, reject) => {
+			if (!this._privateKey) return reject(undefined);
+			webcrypto.subtle
+				.decrypt({ name: "RSA-OAEP" }, this._privateKey, data)
+				.then((encryptedData: ArrayBuffer) => {
+					resolve(encryptedData);
+				})
+				.catch((e) => reject(e));
+		});
+	}
+
+	public arrayBufferToString(buffer: ArrayBuffer) {
+		var binary = "";
+		var bytes = new Uint8Array(buffer);
+		var len = bytes.byteLength;
+		for (var i = 0; i < len; i++) {
+			binary += String.fromCharCode(bytes[i]);
+		}
+		return binary;
+	}
+
+	private formatAsPem(str: string): string {
+		const header = "-----BEGIN PUBLIC KEY-----\n";
+		const footer = "-----END PUBLIC KEY-----";
+		let content = "";
+		while (str.length > 0) {
+			content += str.substring(0, 64) + "\n";
+			str = str.substring(64);
+		}
+
+		return header + content + footer;
+	}
+
+	/**
+	 * Returns human readable PEM
+	 */
+	public spkiToPEM(keydata: ArrayBuffer): string {
+		var keydataS = this.arrayBufferToString(keydata);
+		var keydataB64 = btoa(keydataS);
+		var keydataB64Pem = this.formatAsPem(keydataB64);
+		return keydataB64Pem;
+	}
+}
---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import { E2EEClient } from "./EncrypterClient";
+
+(async () => {
+	const message = "Yesss";
+	const client1 = new E2EEClient();
+	await client1.init();
+	const expotedKey1 = await client1.exportPublicKey();
+
+	const client2 = new E2EEClient();
+	await client2.init();
+	const expotedKey2 = await client2.exportPublicKey();
+	await client2.loadClientPublic(expotedKey1 as ArrayBuffer);
+	const client2Encrypted = await client2.encrypt(Buffer.from(message));
+
+	const client1Decrypted = await client1.decrypt(
+		client2Encrypted as ArrayBuffer
+	);
+	console.log(client1.arrayBufferToString(client1Decrypted as ArrayBuffer));
+})();
