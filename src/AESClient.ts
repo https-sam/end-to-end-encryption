cb8501b69da472a210bd24f277a0ed5ff591a9c1
Sam Goto <osgo1115@icloud.com>
2023-03-30 10:32:26 -0600

    Resolve eslint error

--- src/AESClient.ts
+++ src/AESClient.ts
@@ -131,7 +131,7 @@ export class AESClient {
 	public arrayBufferToString(buffer: ArrayBuffer) {
 		let binary = "";
 		const bytes = new Uint8Array(buffer);
-		for (var = 0; i < bytes.byteLength; i++) {
+		for (let i = 0; i < bytes.byteLength; i++) {
 			binary += String.fromCharCode(bytes[i]);
 		}
 		return binary;
